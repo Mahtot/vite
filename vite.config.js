18d07ade6886f1483ae8db5530c4f47b71622986
[mahtot] <[mahtotgher13@gmail.com]>
2023-11-21 15:03:04 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/vite-app/",
+  base: "/vite-app/",
   plugins: [react()],
   server: {
     port: 3000,
