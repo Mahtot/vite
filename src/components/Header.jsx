08fc6dc0bcbbbfd1b0cabd60cdb2ae27ec266d78
[mahtot] <[mahtotgher13@gmail.com]>
2023-11-21 14:31:07 +0300

    Added files

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <>
+    {import.meta.env.VITE_API}
+      This is from Header this one is added
+      {console.log(import.meta.env)}
+    </>
+  )
+}
+export default Header
