e7eb794f9a799dedf0bd0528632392b5513827a4
[mahtot] <[mahtotgher13@gmail.com]>
2023-12-26 14:38:47 +0300

    Added navigation bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
+import { Routes, Route, Link } from "react-router-dom"
+import Home from "./Home"
+import About from "./About"
+import Projects from "./Projects.jsx"
+import Contact from "./Contact"
 const Header = () => {
   return (
-    <>
-    {import.meta.env.APP_DBNAME}
-      This is from Header this one is added
 
-      why do I hate my life?
-      {console.log(import.meta.env)}
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+        
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<Home/>} ></Route>
+        <Route path='/about' element={<About/>} ></Route>
+        <Route path='/projects' element={<Projects/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+      </Routes>
     </>
   )
 }
